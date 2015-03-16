a5d80d871b216be603ec0138182546f7bb9e136d
Nickson Santos Endlich <nickson12@gmail.com>
2015-03-16 02:38:00 -0300

    Transposed
    This function receives a matrix as entry and returns a transposed
    matrix.

--- transposed.py
+++ transposed.py
@@ -1,9 +1,11 @@
 #Nickson Santos Endlich
+# -*- coding: utf-8 -*-
 # transposed.py
 #-----------------------
 
 def transposed(matrix): #return a transposed matrix
      newmatrix = []
+     numcolumns = len(matrix[0])
      numlines = len(matrix)
      
      for j in range(numcolumns): #browse through the columns of the matrix
@@ -21,4 +23,3 @@ def transposed(matrix): #return a transposed matrix
      return newmatrix
 
 #fim transposed
-
